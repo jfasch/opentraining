4e7f82504ed0ec2f6bce0c8b2eda01cc53491007
Joerg Faschingbauer <jf@faschingbauer.co.at>
2021-11-14 17:24:31 +0100

    bump version to 0.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='opentraining',
-    version='0.4.2',
+    version='0.4.3',
     description='A set of Sphinx extensions to manage training material',
 
     # long_description='blah',
